97a92225d780b15cf2994ce5862f422b11d5e779
shunfujino <w.ss1.c.s1450@gmail.com>
2021-08-22 23:35:43 +0900

    CRUD処理実装。バリデーションなし。

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
-    validates :title, {presence: true}
+    # validates :title, {presence: true}
 
-    # validates :start_date, :presence => true
-    # validates :end_date, :presence => true
+    # validates :start_date, {presence: true}
+    # validates :end_date, {presence: true}
     
 
      
